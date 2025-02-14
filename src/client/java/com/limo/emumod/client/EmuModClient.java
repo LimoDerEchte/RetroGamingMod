21698b0e9c25b852dce102855a90a21945233c23
Limo <mail@vogel-julian.de>
2025-02-14 21:13:09 +0100

    Initial Project Setup Start porting stuff over now

---
+++ src/client/java/com/limo/emumod/client/EmuModClient.java
@@ -0,0 +1,10 @@
+package com.limo.emumod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmuModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/limo/emumod/EmuMod.java
@@ -0,0 +1,10 @@
+package com.limo.emumod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EmuMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
