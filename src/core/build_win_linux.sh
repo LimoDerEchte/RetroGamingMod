cae688d8a73065c0df6deeef479037108f2e6df5
Limo <mail@vogel-julian.de>
2025-03-10 21:40:25 +0100

    Kinda works ig Have to fix all code base issues now

--- src/core/build_win_linux.sh
+++ src/core/build_win_linux.sh
@@ -16,7 +16,7 @@ if [ ! -d "jdk-21" ]; then
   unzip openjdk-21+35_windows-x64_bin.zip
   rm openjdk-21+35_windows-x64_bin.zip
 fi
-JAVA="realpath jdk-21/"
+JAVA=$(realpath jdk-21/)
 cd .. || exit
 
 # Build Windows
@@ -25,19 +25,14 @@ cd win_x64 || exit
 if [ ! -f "bridge/bridge.dll" ]; then
   x86_64-w64-mingw32-cmake -G "Ninja" \
         -DCMAKE_TOOLCHAIN_FILE=../../vcpkg/scripts/buildsystems/vcpkg.cmake \
-        -DJAVA_HOME="$JAVA" \
         -DJAVA_INCLUDE_PATH="$JAVA"/include \
         -DJAVA_INCLUDE_PATH2="$JAVA"/include/win32 \
-        -DJAVA_AWT_INCLUDE_PATH="$JAVA"/include/win32 \
-        -DJAVA_JVM_LIBRARY="$JAVA"/lib/jvm.lib \
-        -DJAVA_AWT_LIBRARY="$JAVA"/lib/jawt.lib \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_SYSTEM_NAME=Windows \
         -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
         -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
         -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres \
         -DVCPKG_TARGET_TRIPLET=x64-mingw-static \
-        -DVCPKG_CMAKE_SYSTEM_NAME=MinGW \
         -DCMAKE_FIND_ROOT_PATH=/usr/x86_64-w64-mingw32 \
         ../..
   ninja || echo "Windows x64 build failed, continuing..."
