e95b36255067f6a35d2eaef295d26b9b12749413
Limo <mail@vogel-julian.de>
2025-12-22 17:38:52 +0100

    Migrate Components

--- src/main/java/com/limo/emumod/registry/EmuComponents.java
+++ src/main/java/com/limo/emumod/registry/EmuComponents.java
@@ -1,6 +1,8 @@
 package com.limo.emumod.registry;
 
+import com.limo.emumod.components.GameComponent;
 import com.mojang.serialization.Codec;
+import net.fabricmc.fabric.api.item.v1.ComponentTooltipAppenderRegistry;
 import net.minecraft.component.ComponentType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.codec.PacketCodecs;
@@ -8,7 +10,6 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Uuids;
-import net.minecraft.util.dynamic.Codecs;
 
 import java.util.UUID;
 import java.util.function.UnaryOperator;
@@ -19,17 +20,16 @@ public class EmuComponents {
     public static final ComponentType<Integer> PORT_NUM = register("port_num", builder -> builder
             .codec(Codec.INT).packetCodec(PacketCodecs.VAR_INT));
 
+    public static final ComponentType<GameComponent> GAME = register("game", builder -> builder
+            .codec(GameComponent.CODEC).packetCodec(GameComponent.PACKET_CODEC));
     public static final ComponentType<ItemStack> CARTRIDGE = register("cartridge", builder -> builder
             .codec(ItemStack.CODEC).packetCodec(ItemStack.PACKET_CODEC));
-    public static final ComponentType<String> GAME = register("game", builder -> builder
-            .codec(Codecs.ESCAPED_STRING).packetCodec(PacketCodecs.STRING));
-    public static final ComponentType<UUID> FILE_ID = register("file_id", builder -> builder
-            .codec(Uuids.CODEC).packetCodec(Uuids.PACKET_CODEC));
 
     public static <T> ComponentType<T> register(String path, UnaryOperator<ComponentType.Builder<T>> builderOperator) {
         return Registry.register(Registries.DATA_COMPONENT_TYPE, Identifier.of("emumod", path), builderOperator.apply(ComponentType.builder()).build());
     }
 
     public static void init() {
+        ComponentTooltipAppenderRegistry.addFirst(GAME);
     }
 }
