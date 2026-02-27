56a41a14a9f71d6900bcc2aa3d19459d046a6364
Limo <mail@vogel-julian.de>
2026-02-27 09:33:18 +0100

    audio en/decode traits

--- core/retro-bridge/src/codec/audio_decoder.rs
+++ core/retro-bridge/src/codec/audio_decoder.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
 
 pub trait AudioDecoder {
+    fn new() -> Self where Self: Sized;
+    fn decode_frame(&mut self, data: Vec<u8>) -> Result<Vec<u16>, Box<dyn Error>>;
 }
--- core/retro-bridge/src/codec/audio_encoder.rs
+++ core/retro-bridge/src/codec/audio_encoder.rs
@@ -1,3 +1,6 @@
+use std::error::Error;
 
 pub trait AudioEncoder {
+    fn new() -> Self where Self: Sized;
+    fn encode_frame(&mut self, data: Vec<u16>) -> Result<Vec<u8>, Box<dyn Error>>;
 }
